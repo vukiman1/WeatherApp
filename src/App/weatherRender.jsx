89c9d5d7c9220a04ae696f7d96526e12ba4a43ec
Sinh vien(pass:123456) <anvu734@gmail..com>
2023-06-22 10:46:41 +0700

    update_22_06_2023

--- src/App/weatherRender.jsx
+++ src/App/weatherRender.jsx
@@ -32,19 +32,18 @@ function Weather(forecast ) {
    //  console.log(typeof(forecast))
    //  console.log(Array.isArray(apiday))
     const forecast2 = Object.values(forecast);
-    console.log(Array.isArray(forecast2))
-    console.log(forecast2.main)
-    console.log(forecast)
+   //  console.log(Array.isArray(forecast2))
+   //  console.log(forecast.forecast[0].dt_txt)
 
     return (
       <>
                <div className="weather-api">
                   {
-                     apiday.map((item) => (
+                     forecast.forecast.map((item) => (
                         <div className="weather-api-day">
-                        <h3 className="weather-api-day-text"></h3>
-                        <img className = 'weather-api-day-img'src='{item.img} 'alt="" />
-                        <h3 className="weather-api-day-text weather-api-day-temperature"> °C</h3>
+                        <h3 className="weather-api-day-text"> {item.dt_txt}</h3>
+                        <img className = 'weather-api-day-img'src={cloud} alt="" />
+                        <h3 className="weather-api-day-text weather-api-day-temperature">{item.main.temp}°C</h3>
                      </div>   
                      ))
                   }                                          
